46749cb4f2d3af78f50e378d1a168af9da818ef5
Bolojan Andrei <alexandru.andreibolojan@gmail.com>
2023-08-07 02:53:38 +0300

    added the main page heading and blogpost component

--- app/(application)/page.js
+++ app/(application)/page.js
@@ -1,3 +1,26 @@
+import BlogPost from "@/components/main-page/BlogPost";
+import Heading from "@/components/main-page/Heading";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex flex-col">
+      <Heading />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-8 gap-x-5 lg:grid-cols-3">
+        <BlogPost
+          id={"2"}
+          title={"Title"}
+          desc={"Desc"}
+          createdAt={"28/04.2020"}
+          tags={["#123", "#cevaFain"]}
+        />
+        <BlogPost
+          id={"2"}
+          title={"Title"}
+          desc={"Desc"}
+          createdAt={"28/04.2020"}
+          tags={["#123", "#cevaFain"]}
+        />
+      </div>
+    </div>
+  );
 }
