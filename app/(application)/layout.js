f48e1011f6eb9e44be1ed18fb3ca657f5b88105f
Bolojan Andrei <alexandru.andreibolojan@gmail.com>
2023-08-07 02:57:53 +0300

    added react-query and the ReactQueryProvider

--- app/(application)/layout.js
+++ app/(application)/layout.js
@@ -2,6 +2,7 @@ import "../globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/Navbar";
 import Provider from "@/lib/Provider";
+import ReactQueryProvider from "@/lib/ReactQueryProvider";
 
 export const metadata = {
   title: "Bloggy",
@@ -12,13 +13,15 @@ export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
       <body className="flex flex-col w-[full]">
-        <Provider session={session}>
-          <div className=" flex flex-col w-full md:w-[80%] mx-auto p-6 md:p-4">
-            <Navbar />
-            {children}
-            <Toaster />
-          </div>
-        </Provider>
+        <ReactQueryProvider>
+          <Provider session={session}>
+            <div className=" flex flex-col w-full md:w-[80%] mx-auto p-6 md:p-4">
+              <Navbar />
+              {children}
+              <Toaster />
+            </div>
+          </Provider>
+        </ReactQueryProvider>
       </body>
     </html>
   );
