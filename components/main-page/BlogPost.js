27bc55b0ed78874bcaafccf1b3494120d883a51c
Bolojan Andrei <alexandru.andreibolojan@gmail.com>
2023-08-07 18:04:08 +0300

    updated the visuals for the blogpost

--- components/main-page/BlogPost.js
+++ components/main-page/BlogPost.js
@@ -5,24 +5,32 @@ import Link from "next/link";
 function BlogPost({ id, title, desc, createdAt, tags, img }) {
   return (
     <div>
-      <Link href={id} className="flex flex-col">
-        <div className="min-w-full min-h-96 max-h-[22rem] max-w-full">
-          <img className="object-cover h-[20rem] w-full" src={img} />
-        </div>
-        <div className="flex flex-col mt-3 ">
-          <div>
-            <p className="font-semibold text-sm text-slate-800">{createdAt}</p>
+      <Link href={id} className="flex flex-col justify-between h-full">
+        <div>
+          <div className="min-w-full min-h-96 max-h-[22rem] max-w-full">
+            <img className="object-cover h-[20rem] w-full" src={img} />
           </div>
-          <h1 className="font-bold text-lg ">{title}</h1>
-          <h2 className="font-medium text-sm">{desc.slice(0, 140) + "..."}</h2>
+          <div className="flex flex-col mt-3 ">
+            <div>
+              <p className="font-semibold text-sm text-slate-800">
+                {createdAt}
+              </p>
+            </div>
+            <h1 className="font-bold text-lg ">{title}</h1>
+            <h2 className="font-medium text-sm">
+              {desc.slice(0, 140) + "..."}
+            </h2>
+          </div>
         </div>
-        <div className="w-full flex justify-start mt-3">
-          <div className="flex space-x-2">
-            {tags.map((tag) => (
-              <Badge key={tag} variant="outline">
-                {tag}
-              </Badge>
-            ))}
+        <div className="flex justify-end mt-3">
+          <div className="w-full flex">
+            <div className="flex space-x-2">
+              {tags.map((tag) => (
+                <Badge key={tag} variant="outline">
+                  {tag}
+                </Badge>
+              ))}
+            </div>
           </div>
         </div>
       </Link>
